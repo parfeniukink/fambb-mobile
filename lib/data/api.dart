4106bfbf222fb15842d3a94bced5c7420b4206ad
Dmytro Parfeniuk <parfeniukink@gmail.com>
2024-11-07 10:57:52 +0200

    🔥 print is removed

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -156,7 +156,6 @@ class ApiService {
   // Returns `false` if not created
   Future<bool> addCost(CostCreateBody costCreateBody) async {
     try {
-      print(costCreateBody.toJson());
       var response = await http.post(
         Uri.parse(
           "$baseUrl$costPath",
